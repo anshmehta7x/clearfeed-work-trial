392f13e8a67038dd72fecc8b0c97e4aed83eafe9
Ansh Mehta <anshkapilmehta@gmail.com>
2026-07-24 12:35:27 +0530

    feat(api): Init project for api with typescript, express and postgres

---
+++ api/src/index.ts
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (_, res) => {
+  res.json({
+    message: "Hello World"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}`);
+});
